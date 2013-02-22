0533a4fa6739810b0c144e606cccaca0272179a3
Donall <donall@donall-laptop.(none)>
2013-02-21 22:07:59 -0800

    Resolving conflicts

--- project.clj
+++ project.clj
@@ -6,8 +6,17 @@
                  [factual/sosueme "0.0.14"]
                  [cheshire "4.0.1"]
                  [ring "1.1.6"]
+                 [org.codehaus.jackson/jackson-core-asl "1.9.9"]
+                 [org.codehaus.jackson/jackson-mapper-asl "1.9.9"]
+                 [org.apache.httpcomponents/httpcore "4.2.2"]
+                 [commons-codec "1.6"]
+                 [commons-logging "1.1.1"]
+                 [commons-httpclient "3.1"]
+                 [org.apache.httpcomponents/httpclient "4.2.3"]
                  [ring/ring-jetty-adapter "1.1.6"]]
   :ring {:handler hallacy.core/handler}
   :plugins [[lein-ring "0.8.2"]]
   :main hallacy.core
+  :source-paths      ["src/clojure"]
+  :java-source-paths ["src/java"]
   )
