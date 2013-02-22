36919ea79e4ccc03dd1e6b8676dba88aabe65c0e
Aaron Crow <dirtyvagabond@yahoo.com>
2013-02-21 18:16:45 -0800

    set min lein to 2

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject hallacy "0.0.1"
   :description "Webservice for Chris Hallacy's Factual Hackathon project"
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [factual/factual-clojure-driver "1.3.1"]
                  [factual/sosueme "0.0.14"]
