d36fc9fbd5a853ca7c6824930a93fd2822db4ca2
Aaron Crow <dirtyvagabond@yahoo.com>
2013-02-21 16:58:48 -0800

    switch from factql to factual-clojure-driver

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject hallacy "0.0.1"
   :description "Webservice for Chris Hallacy's Factual Hackathon project"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [factql "1.0.3"]
+                 [factual/factual-clojure-driver "1.3.1"]
+                 [factual/sosueme "0.0.14"]
                  [cheshire "4.0.1"]
                  [ring/ring-jetty-adapter "1.1.0"]])
--- src/hallacy/core.clj
+++ src/hallacy/core.clj
@@ -1,39 +1,49 @@
 (ns hallacy.core
-  (:require [factql.core :as facts]
-            [cheshire.core :as json])
-  (:use
-   [ring.adapter.jetty :only [run-jetty]]
-   [ring.middleware.params]))
+  (:require [factual.api :as facts]
+            [cheshire.core :as json]
+            [sosueme.conf :as conf])
+  (:use [ring.adapter.jetty :only [run-jetty]]
+        [ring.middleware.params]))
 
-(facts/init!)
+(let [{:keys [key secret]} (conf/dot-factual "factual-auth.yaml")]
+       (facts/factual! key secret))
 
-(defn get-places [lat lon]
-  (facts/select places
-                (around {:lat lat :lon lon :miles 1})))
+(defn get-places [query]
+  (facts/fetch query))
 
 (defn translate-place
   "Translate a place rec from Factual into something usable by mixair"
   [place]
-  {"id" (place :factual_id)
-   "lat" (place :latitude)
-   "lng" (place :longitude)
-   "elevation" "0"
+  {"id"              (place :factual_id)
+   "lat"             (place :latitude)
+   "lng"             (place :longitude)
+   "elevation"       "0"
    "title"           (place :name)
-   ;;"distance"        "3"
    "webpage"         (place :website)
    "has_detail_page" (if (place :website) "1" "0")})
 
 (defn translate [places]
   (map translate-place places))
 
-(defn get-places-body [{lat :lat lon :lon}]
-  ;;JSON
-  (let [places (get-places lat lon)
-        json (json/generate-string
-              {:status "OK"
-               :num_results (count places)
-               :results (translate places)})]
-    json))
+(defn get-places-body [query]
+  (println "get-places-body Q:" query)
+  (let [places (get-places query)]
+    (json/generate-string
+     {:status "OK"
+      :num_results (count places)
+      :results (translate places)})))
+
+;;Factual: longitude=-118.418249&latitude=34.060106
+(defn params->query
+  "Translates raw query params map to a Factual query map"
+  [params]
+  (merge
+   {:geo {:$circle {:$center [(params "latitude"), (params "longitude")]
+                    :$meters 5000}}
+    :table      "restaurants-us"}
+  (when-let [v (params "category")] {:filters {:category_ids {:$eq  v}}})
+  (when-let [v (params "table")]    {:table v})
+  (when-let [v (params "search")]   {:q v})))
 
 (defn handler [{params :params :as req}]
   (println "REQ:" req)
@@ -42,16 +52,7 @@
    :headers {"Content-Type" (if (params "plain")
                               "text/plain"
                               "application/mixare-json")}
-   :body    (get-places-body {:lat (params "latitude")
-                              :lon (params "longitude")})})
-
-#_(defn app [req]
-  (println "REQ:" req)
-  (println "OUT:" JSON)
-  (let [lat (:query-string req)])
-  {:status 200
-   :headers {"Content-Type" "application/mixare-json"}
-   :body JSON})
+   :body    (get-places-body (params->query params))})
 
 (def app
   (wrap-params handler))
